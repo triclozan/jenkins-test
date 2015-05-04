b53d8610ff175afe0f05a8cfb438202d00b57114
Ilushechkin Evgeniy <eugene@pantrylabs.com>
2015-05-04 16:49:33 +0600

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, function (err) {
+}).listen(9337, function (err) {
   console.log(err);
 });
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:9337/');
