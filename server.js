e35b376adc7ba8cebe64f3707bc5e7f10e99bf70
Ilushechkin Evgeniy <eugene@pantrylabs.com>
2015-05-04 16:18:39 +0600

    log error message on server start

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+}).listen(1337, function (err) {
+  console.log(err);
+});
 console.log('Server running at http://127.0.0.1:1337/');
